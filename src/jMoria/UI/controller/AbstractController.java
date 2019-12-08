f2c102509e4ce72fbd6cba7c4a159ed00efa537c
Eric <eric@coubansoftthing.com>
2019-12-07 19:28:31 -0500

    rename controller template to AbstractController add key event handling support to the AbstractController change access restrictions to public

---
+++ src/jMoria/UI/controller/AbstractController.java
@@ -0,0 +1,37 @@
+package jMoria.UI.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import jMoria.UI.SceneManager;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
+
+public abstract class AbstractController {
+
+    @FXML
+    public URL location;
+    @FXML
+    public ResourceBundle resources;
+
+    public SceneManager sceneManager;
+    public EventHandler<KeyEvent> keyEventHandler = this::handleKeys;
+
+    @FXML
+    public abstract void initialize();
+
+    public abstract void start();
+    public abstract void stop();
+
+    public abstract void handleKeys(KeyEvent e);
+
+    public void startEventHandling() {
+        sceneManager.getPrimaryStage().addEventHandler(KeyEvent.ANY, keyEventHandler);
+    }
+
+    public void stopEventHandling() {
+        sceneManager.getPrimaryStage().removeEventFilter(KeyEvent.ANY, keyEventHandler);
+    }
+
+}
