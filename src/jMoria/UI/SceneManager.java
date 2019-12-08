ea5f837b6524933da91cfa2ee27d781dd990c2b8
Eric <eric@coubansoftthing.com>
2019-12-07 19:41:40 -0500

    remove the print statement debugging inject the SceneManager into the primaryController split the constructor into constructor/start methods

--- src/jMoria/UI/SceneManager.java
+++ src/jMoria/UI/SceneManager.java
@@ -1,6 +1,6 @@
 package jMoria.UI;
 
-import jMoria.UI.controller.FXMLControllerTemplate;
+import jMoria.UI.controller.AbstractController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -14,10 +14,10 @@ import java.util.HashMap;
 public class SceneManager extends StackPane {
 
     private Stage primaryStage;
-    private FXMLControllerTemplate primaryController;
+    private AbstractController primaryController;
 
     private HashMap<jMoriaScene, Node> scenes = new HashMap<>();
-    private HashMap<jMoriaScene, FXMLControllerTemplate> controllers = new HashMap<>();
+    private HashMap<jMoriaScene, AbstractController> controllers = new HashMap<>();
 
     public enum jMoriaScene {
         GAME("game.fxml"),
@@ -37,45 +37,54 @@ public class SceneManager extends StackPane {
 
     public SceneManager(Stage primaryStage) {
         super();
-        System.out.println("scenemanager constructor");
         this.primaryStage = primaryStage;
         //this.setStyle("-fx-background-color: #000000");
         this.setStyle("-fx-background-color: #D2691E");
+    }
+
+    public void start() {
         this.primaryStage.setScene(new Scene(this, primaryStage.getWidth(), primaryStage.getHeight()));
         this.setScene(jMoriaScene.SPLASH);
         this.primaryStage.show();
     }
 
     protected void setScene(jMoriaScene scene) {
-        System.out.println("set scene");
+
         // Lazy load the fxml/controller pair.
         if (!scenes.containsKey(scene)) {
             loadScene(scene, scene.getFXMLFile());
         }
-        // Set the scene!
+
         if (primaryController != null) {
             primaryController.stop();
         }
+
         if(!this.getChildren().isEmpty()) {
             this.getChildren().remove(0);
         }
         this.getChildren().add(0, scenes.get(scene));
+
         primaryController = controllers.get(scene);
-        primaryController.init();
+        primaryController.sceneManager = this;
+        primaryController.start();
     }
 
     private void loadScene(jMoriaScene scene, String fxmlFile) {
-        System.out.println("load scene");
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("/jMoria/UI/fxml/" + fxmlFile));
             System.out.println(loader.getLocation());
             Parent root = loader.load();
-            FXMLControllerTemplate controller = loader.getController();
+            AbstractController controller = loader.getController();
             scenes.put(scene, root);
             controllers.put(scene, controller);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
 }
