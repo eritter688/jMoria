ffa6a5a42373baa250cfe9b056e7dd21e924be12
Eric <eritter688@gmail.com>
2020-03-22 18:45:52 -0400

    revisions to AbstractScreen

--- src/jMoria/game/screens/AbstractScreen.java
+++ src/jMoria/game/screens/AbstractScreen.java
@@ -1,16 +1,11 @@
 package jMoria.game.screens;
 
-import java.util.HashSet;
-
 public abstract class AbstractScreen {
 
-    public HashSet<String> acceptedKeys;
+    public abstract void initialize();
 
     public abstract void render();
 
     public abstract void handleInput();
-
-
-
 
 }
