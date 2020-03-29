2f059a6887752b79dbb0b9c08b94ae2675625f03
Eric <eritter688@gmail.com>
2020-03-29 14:29:28 -0400

    where to put the data??

--- src/jMoria/game/living/statics/PlayerCreation.java
+++ src/jMoria/game/living/statics/PlayerCreation.java
@@ -1,9 +1,28 @@
 package jMoria.game.living.statics;
 
+import jMoria.game.enums.Race;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 public class PlayerCreation {
 
-    public static int getPlayerAge() {
-        return 0;
+    public static Map<Race, int[]> baseAgeAndModifier = Map.ofEntries(
+            entry(Race.HUMAN, new int[]{14, 6}),
+            entry(Race.HALF_ELF, new int[]{24, 16}),
+            entry(Race.ELF, new int[]{75, 75}),
+            entry(Race.HALFLING, new int[]{21, 12}),
+            entry(Race.GNOME, new int[]{50, 40}),
+            entry(Race.DWARF, new int[]{35, 15}),
+            entry(Race.HALF_ORC, new int[]{11, 4}),
+            entry(Race.HALF_TROLL, new int[]{20, 10})
+    );
+
+    public static int getPlayerAge(Race race) {
+        int[] baseAndMod = baseAgeAndModifier.get(race);
+
+        return 1;
     }
 
 }
