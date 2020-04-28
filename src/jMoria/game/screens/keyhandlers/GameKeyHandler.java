03d43918a6bde1c7de74d5a1b5dbd3dbc3634848
Eric <eritter688@gmail.com>
2020-04-28 11:00:51 -0400

    testing game key handler

--- src/jMoria/game/screens/keyhandlers/GameKeyHandler.java
+++ src/jMoria/game/screens/keyhandlers/GameKeyHandler.java
@@ -5,6 +5,18 @@ public class GameKeyHandler extends AbstractKeyHandler {
     @Override
     public void handleKey(String key) {
 
+      System.out.println(key);
+
+      switch (key) {
+        case "c":
+          System.out.println("close");
+          break;
+        case "ESCAPE":
+          System.out.println("escape");
+          break;
+      }
+
+
     }
 
 }
