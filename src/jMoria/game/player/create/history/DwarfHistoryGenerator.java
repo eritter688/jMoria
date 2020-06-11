faeef4affc4135493ea4c4c3d1d5b6ad543150cd
Eric <eritter688@gmail.com>
2020-06-11 16:53:30 -0400

    dwarf history gen

---
+++ src/jMoria/game/player/create/history/DwarfHistoryGenerator.java
@@ -0,0 +1,59 @@
+package jMoria.game.player.create.history;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DwarfHistoryGenerator extends AbstractHistoryGenerator {
+
+  public List<HistoryObject> ancestry_one = new ArrayList<>();
+  public List<HistoryObject> ancestry_two = new ArrayList<>();
+  public List<HistoryObject> ancestry_three = new ArrayList<>();
+  public List<HistoryObject> eyes = new ArrayList<>();
+  public List<HistoryObject> hair_one = new ArrayList<>();
+  public List<HistoryObject> hair_two = new ArrayList<>();
+  public List<HistoryObject> beard = new ArrayList<>();
+  public List<HistoryObject> complexion = new ArrayList<>();
+
+  public DwarfHistoryGenerator() {
+
+    ancestry_one.add(new HistoryObject("You are one of two children of a Dwarven ", 40));
+    ancestry_one.add(new HistoryObject("You are the only child of a Dwarven ", 50));
+
+    ancestry_two.add(new HistoryObject("Thief.  ", 60));
+    ancestry_two.add(new HistoryObject("Prison Guard.  ", 75));
+    ancestry_two.add(new HistoryObject("Miner.  ", 90));
+    ancestry_two.add(new HistoryObject("Warrior.  ", 110));
+    ancestry_two.add(new HistoryObject("Priest.  ", 130));
+    ancestry_two.add(new HistoryObject("King.  ", 150));
+
+    ancestry_three.add(new HistoryObject("You are the black sheep of the family.  ", 10));
+    ancestry_three.add(new HistoryObject("You are a credit to the family.  ", 50));
+    ancestry_three.add(new HistoryObject("You are a well liked child.  ", 55));
+
+    eyes.add(new HistoryObject("You have dark brown eyes, ", 50));
+    eyes.add(new HistoryObject("You have glowing red eyes, ", 60));
+
+    hair_one.add(new HistoryObject("straight ", 50));
+    hair_one.add(new HistoryObject("wavy ", 50));
+
+    hair_two.add(new HistoryObject("black hair, ", 50));
+    hair_two.add(new HistoryObject("brown hair, ", 50));
+
+    beard.add(new HistoryObject("a one foot beard, ", 50));
+    beard.add(new HistoryObject("a two foot beard, ", 51));
+    beard.add(new HistoryObject("a three foot beard, ", 53));
+    beard.add(new HistoryObject("a four foot beard, ", 55));
+
+    complexion.add(new HistoryObject("and a dark complexion.", 50));
+
+    historyList.add(ancestry_one);
+    historyList.add(ancestry_two);
+    historyList.add(ancestry_three);
+    historyList.add(eyes);
+    historyList.add(hair_one);
+    historyList.add(hair_two);
+    historyList.add(complexion);
+
+  }
+
+}
